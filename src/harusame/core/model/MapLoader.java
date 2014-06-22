11eb85ed1eb0238b6b20084fa124b0a613503177
Calle <Calle@Calle-Dator.zyxel.com>
2014-06-22 17:17:11 +0200

    Lagt in read level blabla

--- src/harusame/core/model/MapLoader.java
+++ src/harusame/core/model/MapLoader.java
@@ -4,12 +4,58 @@
  * and open the template in the editor.
  */
 
-package harusame.project.core.model;
+package harusame.core.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
  * @author Hikari
  */
-public class MapLoader {
+public class MapLoader 
+{
+    char[] map;
+    
+    
+    public MapLoader(int width, int height)
+    {
+        map = new char[(width*height) / 10];
+    }
+    
+    public char[] readMap(String level)
+    {
+        BufferedReader br = null;
+           
+		try {                     
+			String sCurrentLine;
+                        int counter = 0;
+ 
+			br = new BufferedReader(new FileReader(level));
+ 
+			while ((sCurrentLine = br.readLine()) != null) 
+                        {
+                            for(int i = 0; i < sCurrentLine.length(); i++)
+                            {
+                                map[counter] = sCurrentLine.charAt(i);
+                                counter++;
+                            }
+                            System.out.println(sCurrentLine);
+			}
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+               
+        return map;
+    }
+    
     
 }
