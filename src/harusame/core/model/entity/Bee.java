86b8ed82c5bdcfa5616aef718e453cd0611d0de4
Calle <Calle@Calle-Dator.zyxel.com>
2014-06-30 16:32:34 +0200

    Fixade animationsjitter

--- src/harusame/core/model/entity/Bee.java
+++ src/harusame/core/model/entity/Bee.java
@@ -2,6 +2,7 @@ package harusame.core.model.entity;
 
 
 import harusame.core.model.animation.PlayerAnimationLoader;
+import harusame.core.util.Direction;
 
 import static harusame.core.util.Direction.DOWN;
 import static harusame.core.util.Direction.LEFT;
@@ -16,18 +17,19 @@ import static harusame.core.util.Direction.UP;
 public class Bee extends Sprite {
     
     private final PlayerAnimationLoader   pal = new PlayerAnimationLoader("Enemies/th_bee.png");
-    
+    private Direction lastDirection;
    
     public Bee(int x, int y) {
         super(x, y);
         direction = LEFT;
         setAnimation (pal.getFacingLeft());
-        MOVE_SPEED = 1;
+        MOVE_SPEED = 3;
     }
     
     public void revert () {  
         x = lastX;
         y = lastY;       
+        lastDirection = direction;
         
         switch (direction){
             case LEFT:  
@@ -51,27 +53,40 @@ public class Bee extends Sprite {
     @Override
     public void update () {
         lastX = x;
-        lastY = y;         
+        lastY = y;   
+        
 
         switch (direction){
             case LEFT:                 
                 this.x-=MOVE_SPEED;
-                setAnimation (pal.getFacingLeft());
+                if(lastDirection == direction)
+                    setAnimation (pal.getFacingLeft());
+                else
+                    lastDirection = direction;
                 break;
                 
             case RIGHT: 
                 this.x+=MOVE_SPEED;
-                setAnimation (pal.getFacingRight());           
+                if(lastDirection == direction)
+                    setAnimation (pal.getFacingRight());
+                else
+                    lastDirection = direction;        
                 break;
                 
             case UP: 
                 this.y-=MOVE_SPEED;
-                setAnimation (pal.getFacingUp());
+                if(lastDirection == direction)
+                    setAnimation (pal.getFacingUp());
+                else
+                    lastDirection = direction;
                 break;
                 
             case DOWN: 
                 this.y+=MOVE_SPEED;
-                setAnimation (pal.getFacingDown());
+                if(lastDirection == direction)
+                    setAnimation (pal.getFacingDown());
+                else
+                    lastDirection = direction;
                 break;
                 
             case NEUTRAL: 
